855922d669d262fe82e14683a1df134c3fff362d
Igor Klein <igor.klein.sl@gmail.com>
2023-01-25 05:36:23 -0300

    initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,22 @@
+import './styles/global.css'
+
+// import { Habit } from "./components/Habit"
+
+import logoImage from './assets/logo.svg'
+import { Plus } from 'phosphor-react'
+import { Header } from './components/Header'
+
+export function App() {
+
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>          
+        
+        <Header />
+
+      </div>
+    </div>
+
+  )
+}
+
---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,9 @@
+export function SummaryTable() {
+    return (
+        <div className="w-full flex">
+            <div className="grid grid-rows-6"></div>
+
+        </div>
+
+    )
+}
