247faef0b74629adb2fa3aff539fd37929963392
Igor Klein <igor.klein.sl@gmail.com>
2023-02-11 12:55:18 -0300

    mobile: HabitDay screen completed

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { Text, View, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { generateDatesFromYearsBeginning } from "../utils/generateDatesFromYearsBeginning";
 
@@ -11,6 +12,9 @@ const minimumSummaryDatesSize = 18 * 5
 const amountOfDaysToFill = minimumSummaryDatesSize - datesFromYearStart.length
 
 export function Home() {
+
+    const { navigate } = useNavigation();
+
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
@@ -38,6 +42,7 @@ export function Home() {
                     {datesFromYearStart.map((date, i) => (
                         <HabitDay 
                             key={date.toISOString()}
+                            onPress={() => navigate( 'habit', { date: date.toISOString() } )}
                         />
                     ))}
 
